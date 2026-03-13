2441a6716a6a82549b6d33fff0e9bf58b0680bae
Aengus MacKenzie <aengus.2000@gmail.com>
2026-03-13 15:13:49 -0400

    added main menu to gameLogic.js

--- src/gameLogic.js
+++ src/gameLogic.js
@@ -1,2 +1,42 @@
+import chalk from "chalk";
 import questions from "./quizData.js";
-import gameState from "./gameState.js";
+import { select } from "@inquirer/prompts";
+
+//Main Menu
+export async function showMainMenu(gameState) {
+  const action = await select({
+    message: "Main Menu",
+    choices: [
+      { name: "Start Quiz", value: "start" },
+      { name: "See Score", value: "stats" },
+      { name: "Reset Score", value: "reset" },
+      { name: "Quit", value: "quit" },
+    ],
+  });
+
+  switch (action) {
+    case "start":
+      await startQuiz(gameState);
+      break;
+    case "stats":
+      showScore(gameState);
+      await select({
+        message: "Press Enter to go back",
+        choices: [{ name: "Back", value: "back" }],
+      });
+      await showMainMenu(gameState);
+      break;
+    case "reset":
+      resetQuiz(gameState);
+      console.log(chalk.blue("Score has been reset."));
+      await showMainMenu(gameState);
+      break;
+    case "quit":
+      console.log("Goodbye!");
+      process.exit(0);
+  }
+}
+
+//export async function startQuiz(gameState) {
+
+//}
